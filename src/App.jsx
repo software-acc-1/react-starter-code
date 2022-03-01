02243c15046ce54f0ac7959e5e8918618c6b7a0c
shelf <mohamedsaidlearn@gmail.com>
2022-03-01 10:12:38 +0200

    starter-code initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      Hello world
+      <img
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.explicit.bing.net%2Fth%3Fid%3DOIP.nmY2o1MdUEMRBhFkRVxkyAHaGF%26pid%3DApi&f=1"
+        alt="h"
+      />
+    </div>
+  );
+}
+
+export default App;
